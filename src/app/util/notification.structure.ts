b27f80ef8be3afd29f019750c5a147704fa53d54
Anatole <me@anatole.dev>
2021-07-07 01:17:01 +0200

    NotificationStructure Part

--- src/app/util/notification.structure.ts
+++ src/app/util/notification.structure.ts
@@ -1,8 +1,12 @@
 import { DataStructure } from '@typings/typings/DataStructure';
+import { DataService } from 'src/app/service/data.service';
 import { Structure } from 'src/app/util/abstract.structure';
+import { EmoteStructure } from 'src/app/util/emote.structure';
+import { UserStructure } from 'src/app/util/user.structure';
 
 export class NotificationStructure extends Structure<'notification'> {
 	id = '';
+	parts = [] as NotificationStructure.MessagePart[];
 
 	pushData(data: DataStructure.Notification): this {
 		if (!data) {
@@ -12,6 +16,24 @@ export class NotificationStructure extends Structure<'notification'> {
 		if (typeof data.id === 'string') {
 			this.id = data.id;
 		}
+
+		// Cache users in the notification data
+		if (Array.isArray(data.users) && data.users.length > 0) {
+			this.dataService.add('user', ...data.users);
+		}
+		// Cache emotes in the notification data
+		if (Array.isArray(data.emotes) && data.emotes.length > 0) {
+			this.dataService.add('emote', ...data.emotes);
+		}
+
+		// Add notification parts
+		if (Array.isArray(data.message_parts) && data.message_parts.length > 0) {
+			this.parts = [];
+			for (const p of data.message_parts) {
+				this.parts.push(new NotificationStructure.MessagePart(p, this.dataService));
+			}
+		}
+
 		const newData = { ...this.data.getValue() } as DataStructure.Notification;
 		for (const k of Object.keys(data)) {
 			const key = k as keyof DataStructure.Notification;
@@ -32,5 +54,51 @@ export class NotificationStructure extends Structure<'notification'> {
 }
 
 export namespace NotificationStructure {
+	export class MessagePart {
+		constructor(
+			private data: DataStructure.Notification.MessagePart,
+			private dataService: DataService
+		) {
 
+		}
+
+		/**
+		 * Whether the part type is text
+		 */
+		isText(): boolean {
+			return this.data.type === DataStructure.Notification.MessagePartType.TEXT;
+		}
+
+		/**
+		 * Whether the part type mentions a user
+		 */
+		isUserMention(): boolean {
+			return this.data.type === DataStructure.Notification.MessagePartType.USER_MENTION;
+		}
+
+		/**
+		 * Whether the part type mentions an emote
+		 */
+		isEmoteMention(): boolean {
+			return this.data.type === DataStructure.Notification.MessagePartType.EMOTE_MENTION;
+		}
+
+		getText(): string {
+			return this.data.data ?? '';
+		}
+
+		/**
+		 * Get the mentioned user
+		 */
+		getUser(): UserStructure {
+			return this.dataService.get('user', { id: this.data.data as string })[0];
+		}
+
+		/**
+		 * Get the mentioned emote
+		 */
+		getEmote(): EmoteStructure {
+			return this.dataService.get('emote', { id: this.data.data as string })[0];
+		}
+	}
 }
